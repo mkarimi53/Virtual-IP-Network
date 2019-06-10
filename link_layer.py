0f757007fa29a86c472dc6a6c88ecd903b99db89
fateme <fatemejahangiri1377@gmail.com>
2019-06-11 00:22:38 +0430

    not workinng yet

--- link_layer.py
+++ link_layer.py
@@ -1,50 +1,57 @@
 import socket, threading,sys,selectors
-import node_info
+from node_info import Link_info,Node_info
 class linklayer:
-    __init__(self,port,bufferSize):
-        self.localport=port
-        self.interfaces=interfaces
-        self.localip="127.0.0.1"
+    def __init__(self,port,bufferSize):
+        self.localPort=port
+        self.localIP="127.0.0.1"
         self.bufferSize=bufferSize
-        self.UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        self.DPServerSocket.bind((self.localIP, self.localPort))
+        self.UDPSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        
+        self.UDPSocket.bind((self.localIP, self.localPort))
+        self.UDPSocket.setblocking(False)
         self.recieving=True
     
-    sendData(self,srcIP,dstIP,port):
-        UDPClientSocket.sendto(bytesToSend, (dstIP,port))
+    def sendData(self,port,data):
+        self.UDPSocket.sendto(data.encode(), (self.localIP,port))
     
-    recievingData(self):
-        while(recieving):
-            msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    def recievingData(self):
+        print("recieving")
+        while(self.recieving):
+            msgFromServer = self.UDPSocket.recvfrom(self.bufferSize)
+            print(msgFromServer.decode())
+
  
 class linklayerThreaded(threading.Thread):
-    __init__(self,port,bufferSize):
+    def __init__(self,UDPSocket,bufferSize):
         threading.Thread.__init__(self)
-        self.localport=port
-        self.interfaces=interfaces
-        self.localip="127.0.0.1"
+        self.localIP="127.0.0.1"
         self.bufferSize=bufferSize
-        self.UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        self.DPServerSocket.bind((self.localIP, self.localPort))
+        self.UDPSocket = UDPSocket
         self.recieving=True
 
-    sendData(self,srcIP,dstIP,port):
-        UDPClientSocket.sendto(bytesToSend, (dstIP,port))
+    def sendData(self,port,massage):
+        self.UDPSocket.sendto(message.encode(), (self.localIP,port))
 
-    recievingData(self):
-        while(recieving):
-            msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-            print(msgFromServer)
+    def run(self):
+        print("recieving")
+        while(self.recieving):
+            try:
+                msgFromServer = self.UDPSocket.recvfrom(self.bufferSize)
+            except socket.error as r:
+                print("i got error")
+                print(r)
+            print(msgFromServer.decode())
 
 
 class Node:
-    __init__(self,filePath):
+    def  __init__(self,filePath):
         self.commandList=["interfaces","routes","down","up","send","q","traceroute"]
         self.node_info=Node_info(filePath)
         self.lk=linklayer(self.node_info.lport,100)
 
 
-    run(self):
+    def run(self):
+        print("hey")
         while(True):
             commandraw=input(">")
             command=commandraw.split(" ")
@@ -58,8 +65,8 @@ class Node:
                 print(self.commandList[3])
             elif(command[0]==self.commandList[4]):#send
                 interfaceNum=int(command[1])
-                node_info.send()
-                self.node_info.remotes[interfaceNum].remote_port
+                node_info.send(command[2],self.node_info.remotes[interfaceNum].remote_port)
+                
                 print(self.commandList[4])
             elif(command[0]==self.commandList[5]):#q
                 print(self.commandList[5])
@@ -68,7 +75,8 @@ class Node:
         
 if __name__=="__main__":
     
-    node=Node()
-    nodeThread=linklayerThreaded(node.node_info.lport,100)
-    nodeThread.recievingData()
+    node=Node(sys.argv[1])
+    nodeThread=linklayerThreaded(node.lk.UDPSocket,10)
+    nodeThread.run()
+    print("what the hell")
     node.run()
